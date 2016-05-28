278c79e9fe7bf2aa21e817a0c5499122ab03b61a
ACPLMaverick <MaverickCCCP@Gmail.com>
2016-05-28 22:19:01 +0200

    [+] Szajerman's wiimote lib full support (flight & all input). It works in editor as well as in build and it seems to give out less error than previous lib [-] Removed crappy NUI Wiimote plugin and its support

--- Paraglider/Source/Paraglider/Private/WiimoteWrapperComponent.cpp
+++ Paraglider/Source/Paraglider/Private/WiimoteWrapperComponent.cpp
@@ -23,9 +23,17 @@ void UWiimoteWrapperComponent::BeginPlay()
 	float time = GetTimeSeconds();
 	float newTime;
 	bool connected;
+
+	_remote.ChangedCallback = OnRemoteStateChanged;
+	_remote.CallbackTriggerFlags = (state_change_flags)(CONNECTED |
+		WIIMOTE_CHANGED |
+		EXTENSION_CHANGED |
+		MOTIONPLUS_CHANGED);
+	_remote.wrapperRef = this;
+
 	do
 	{
-		connected = _remote.Connect(wiimote::FIRST_AVAILABLE);
+		connected = _remote.Connect(wiimote::FIRST_AVAILABLE, false);
 		newTime = GetTimeSeconds();
 	} while (!connected && (newTime - time <= CONNECTION_WAIT_TIME_SECONDS));
 
@@ -34,16 +42,9 @@ void UWiimoteWrapperComponent::BeginPlay()
 		DEBUG_LOG("Wiimote found!");
 
 		_remote.SetLEDs(1 | (1 << 3));
-		_remote.ChangedCallback = OnRemoteStateChanged;
-		_remote.CallbackTriggerFlags = (state_change_flags)(CONNECTED |
-			WIIMOTE_CHANGED |
-			EXTENSION_CHANGED |
-			MOTIONPLUS_CHANGED);
-		if (_remote.MotionPlusConnected())
-		{
-			_remote.EnableMotionPlus();
-		}
-		_remote.wrapperRef = this;
+		_remote.RefreshState();
+		
+		_prevState = (wiimote_state)_remote;
 	}
 	else
 	{
@@ -57,7 +58,10 @@ void UWiimoteWrapperComponent::TickComponent( float DeltaTime, ELevelTick TickTy
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	if (_remote.IsConnected())
+	{
+		state_change_flags sc = _remote.RefreshState();
+	}
 }
 
 void UWiimoteWrapperComponent::BeginDestroy()
@@ -78,19 +82,83 @@ inline float UWiimoteWrapperComponent::GetTimeSeconds()
 
 void UWiimoteWrapperComponent::OnRemoteStateChanged(wiimote & remote, state_change_flags changed, const wiimote_state & newState)
 {
+	// from example code
+
+			if (changed & CONNECTED)
+			{
+				// ask the wiimote to report everything (using the 'non-continous updates'
+				//  default mode - updates will be frequent anyway due to the acceleration/IR
+				//  values changing):
+
+				// note1: you don't need to set a report type for Balance Boards - the
+				//		   library does it automatically.
+
+				// note2: for wiimotes, the report mode that includes the extension data
+				//		   unfortunately only reports the 'BASIC' IR info (ie. no dot sizes),
+				//		   so let's choose the best mode based on the extension status:
+				if (newState.ExtensionType != wiimote::BALANCE_BOARD)
+				{
+					if (newState.bExtension)
+						remote.SetReportType(wiimote::IN_BUTTONS_ACCEL_IR_EXT); // no IR dots
+					else
+						remote.SetReportType(wiimote::IN_BUTTONS_ACCEL_IR);		//    IR dots
+				}
+			}
+			// a MotionPlus was detected
+			if (changed & MOTIONPLUS_DETECTED)
+			{
+				// enable it if there isn't a normal extension plugged into it
+				// (MotionPlus devices don't report like normal extensions until
+				//  enabled - and then, other extensions attached to it will no longer be
+				//  reported (so disable the M+ when you want to access them again).
+				if (remote.ExtensionType == wiimote_state::NONE) {
+					bool res = remote.EnableMotionPlus();
+				}
+			}
+			// an extension is connected to the MotionPlus
+			else if (changed & MOTIONPLUS_EXTENSION_CONNECTED)
+			{
+				// We can't read it if the MotionPlus is currently enabled, so disable it:
+				if (remote.MotionPlusEnabled())
+					remote.DisableMotionPlus();
+			}
+			// an extension disconnected from the MotionPlus
+			else if (changed & MOTIONPLUS_EXTENSION_DISCONNECTED)
+			{
+				// enable the MotionPlus data again:
+				if (remote.MotionPlusConnected())
+					remote.EnableMotionPlus();
+			}
+			// another extension was just connected:
+			else if (changed & EXTENSION_CONNECTED)
+			{
+				// switch to a report mode that includes the extension data (we will
+				//  loose the IR dot sizes)
+				// note: there is no need to set report types for a Balance Board.
+				if (!remote.IsBalanceBoard())
+					remote.SetReportType(wiimote::IN_BUTTONS_ACCEL_IR_EXT);
+			}
+			// extension was just disconnected:
+			else if (changed & EXTENSION_DISCONNECTED)
+			{
+				// use a non-extension report mode (this gives us back the IR dot sizes)
+				remote.SetReportType(wiimote::IN_BUTTONS_ACCEL_IR);
+			}
+
+	//
 	FVector orientationVector, accelerationVector, speedVector;
 
-	orientationVector.X = remote.Acceleration.Orientation.Pitch;
+	orientationVector.X = newState.Acceleration.Orientation.Pitch;
 	orientationVector.Y = 0.0f;
-	orientationVector.Z = remote.Acceleration.Orientation.Roll;
+	orientationVector.Z = newState.Acceleration.Orientation.Roll;
 
-	accelerationVector.X = remote.Acceleration.X;
-	accelerationVector.Y = remote.Acceleration.Y;
-	accelerationVector.Z = remote.Acceleration.Z;
+	accelerationVector.X = newState.Acceleration.X;
+	accelerationVector.Y = newState.Acceleration.Y;
+	accelerationVector.Z = newState.Acceleration.Z;
 
-	speedVector.X = remote.MotionPlus.Speed.Pitch;
-	speedVector.Y = remote.MotionPlus.Speed.Yaw;
-	speedVector.Z = remote.MotionPlus.Speed.Roll;
+	speedVector.X = newState.MotionPlus.Speed.Pitch;
+	speedVector.Y = newState.MotionPlus.Speed.Yaw;
+	speedVector.Z = newState.MotionPlus.Speed.Roll;
 
 	if (remote.wrapperRef != nullptr)
 	{
@@ -112,15 +180,168 @@ void UWiimoteWrapperComponent::OnRemoteStateChanged(wiimote & remote, state_chan
 			remote.wrapperRef->OnSpeedChanged.Broadcast(speedVector);
 		}
 	}
+
+#pragma region inputEventsCodeshit
+
+	// fire appropriate input events
+	wiimote_state prevState = remote.wrapperRef->_prevState;
+	if (newState.Button.A())
+	{
+		remote.wrapperRef->OnButtonA.Broadcast();
+	}
+	if (newState.Button.B())
+	{
+		remote.wrapperRef->OnButtonB.Broadcast();
+	}
+	if (newState.Button.Up())
+	{
+		remote.wrapperRef->OnButtonDUp.Broadcast();
+	}
+	if (newState.Button.Down())
+	{
+		remote.wrapperRef->OnButtonDDown.Broadcast();
+	}
+	if (newState.Button.Left())
+	{
+		remote.wrapperRef->OnButtonDLeft.Broadcast();
+	}
+	if (newState.Button.Right())
+	{
+		remote.wrapperRef->OnButtonDRight.Broadcast();
+	}
+	if (newState.Button.Home())
+	{
+		remote.wrapperRef->OnButtonHome.Broadcast();
+	}
+	if (newState.Button.Plus())
+	{
+		remote.wrapperRef->OnButtonPlus.Broadcast();
+	}
+	if (newState.Button.Minus())
+	{
+		remote.wrapperRef->OnButtonMinus.Broadcast();
+	}
+	if (newState.Button.One())
+	{
+		remote.wrapperRef->OnButtonOne.Broadcast();
+	}
+	if (newState.Button.Two())
+	{
+		remote.wrapperRef->OnButtonTwo.Broadcast();
+	}
+
+	if (newState.Button.A() && !prevState.Button.A())
+	{
+		remote.wrapperRef->OnButtonAPressed.Broadcast();
+	}
+	if (newState.Button.B() && !prevState.Button.B())
+	{
+		remote.wrapperRef->OnButtonBPressed.Broadcast();
+	}
+	if (newState.Button.Up() && !prevState.Button.Up())
+	{
+		remote.wrapperRef->OnButtonDUpPressed.Broadcast();
+	}
+	if (newState.Button.Down() && !prevState.Button.Down())
+	{
+		remote.wrapperRef->OnButtonDDownPressed.Broadcast();
+	}
+	if (newState.Button.Left() && !prevState.Button.Left())
+	{
+		remote.wrapperRef->OnButtonDLeftPressed.Broadcast();
+	}
+	if (newState.Button.Right() && !prevState.Button.Right())
+	{
+		remote.wrapperRef->OnButtonDRightPressed.Broadcast();
+	}
+	if (newState.Button.Home() && !prevState.Button.Home())
+	{
+		remote.wrapperRef->OnButtonHomePressed.Broadcast();
+	}
+	if (newState.Button.Plus() && !prevState.Button.Plus())
+	{
+		remote.wrapperRef->OnButtonPlusPressed.Broadcast();
+	}
+	if (newState.Button.Minus() && !prevState.Button.Minus())
+	{
+		remote.wrapperRef->OnButtonMinusPressed.Broadcast();
+	}
+	if (newState.Button.One() && !prevState.Button.One())
+	{
+		remote.wrapperRef->OnButtonOnePressed.Broadcast();
+	}
+	if (newState.Button.Two() && !prevState.Button.Two())
+	{
+		remote.wrapperRef->OnButtonTwoPressed.Broadcast();
+	}
+
+	if (!newState.Button.A() && prevState.Button.A())
+	{
+		remote.wrapperRef->OnButtonAReleased.Broadcast();
+	}
+	if (!newState.Button.B() && prevState.Button.B())
+	{
+		remote.wrapperRef->OnButtonBReleased.Broadcast();
+	}
+	if (!newState.Button.Up() && prevState.Button.Up())
+	{
+		remote.wrapperRef->OnButtonDUpReleased.Broadcast();
+	}
+	if (!newState.Button.Down() && prevState.Button.Down())
+	{
+		remote.wrapperRef->OnButtonDDownReleased.Broadcast();
+	}
+	if (!newState.Button.Left() && prevState.Button.Left())
+	{
+		remote.wrapperRef->OnButtonDLeftReleased.Broadcast();
+	}
+	if (!newState.Button.Right() && prevState.Button.Right())
+	{
+		remote.wrapperRef->OnButtonDRightReleased.Broadcast();
+	}
+	if (!newState.Button.Home() && prevState.Button.Home())
+	{
+		remote.wrapperRef->OnButtonHomeReleased.Broadcast();
+	}
+	if (!newState.Button.Plus() && prevState.Button.Plus())
+	{
+		remote.wrapperRef->OnButtonPlusReleased.Broadcast();
+	}
+	if (!newState.Button.Minus() && prevState.Button.Minus())
+	{
+		remote.wrapperRef->OnButtonMinusReleased.Broadcast();
+	}
+	if (!newState.Button.One() && prevState.Button.One())
+	{
+		remote.wrapperRef->OnButtonOneReleased.Broadcast();
+	}
+	if (!newState.Button.Two() && prevState.Button.Two())
+	{
+		remote.wrapperRef->OnButtonTwoReleased.Broadcast();
+	}
+
+#pragma endregion
+
+	// set current state as new
+	remote.wrapperRef->_prevState = newState;
 }
 
-//void UWiimoteWrapperComponent::Tilt(FVector vec)
-//{
-//	return;
-//}
-
-float UWiimoteWrapperComponent::ReturnChuj()
+FVector UWiimoteWrapperComponent::WiimoteTilt()
 {
-	return 3.14f;
+	return FVector(_remote.Acceleration.Orientation.Pitch, 0.0f, _remote.Acceleration.Orientation.Roll);
 }
 
+FVector UWiimoteWrapperComponent::WiimoteRotationRate()
+{
+	return FVector(_remote.MotionPlus.Speed.Pitch, _remote.MotionPlus.Speed.Yaw, _remote.MotionPlus.Speed.Roll);
+}
+
+FVector UWiimoteWrapperComponent::WiimoteAcceleration()
+{
+	return FVector(_remote.Acceleration.X, _remote.Acceleration.Y, _remote.Acceleration.Z);
+}
+
+void UWiimoteWrapperComponent::CalibrateAtRest()
+{
+	_remote.CalibrateAtRest();
+}
--- Paraglider/Source/Paraglider/Public/WiimoteWrapperComponent.h
+++ Paraglider/Source/Paraglider/Public/WiimoteWrapperComponent.h
@@ -17,6 +17,7 @@
 #ifdef ENABLE_DEBUG_LOGS
 
 #define DEBUG_LOG(a) UE_LOG(LogActor, Error, TEXT(a), TEXT(CATEGORY))
+#define DEBUG_LOG_STR(a) UE_LOG(LogActor, Error, (a), TEXT(CATEGORY))
 
 #elif
 
@@ -24,6 +25,7 @@
 
 #endif
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWiimoteButtonSignature);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FWiimoteOneVectorEventSignature, struct FVector, VectorParam);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
@@ -36,6 +38,7 @@ private:
 	const float CONNECTION_WAIT_TIME_SECONDS = 5.0f;
 
 	wiimote _remote;
+	wiimote_state _prevState;
 
 	FVector _prevAcceleration;
 	FVector _prevSpeed;
@@ -43,6 +46,7 @@ private:
 
 	inline float GetTimeSeconds();
 	static void OnRemoteStateChanged(wiimote &remote, state_change_flags changed, const wiimote_state &newState);
+
 public:	
 	// Sets default values for this component's properties
 	UWiimoteWrapperComponent();
@@ -64,9 +68,52 @@ public:
 
 #pragma endregion
 
+#pragma region ButtonEvents
+	
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonA;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonB;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDUp;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDDown;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDLeft;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDRight;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonHome;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonPlus;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonMinus;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonOne;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonTwo;
+
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonAPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonBPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDUpPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDDownPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDLeftPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDRightPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonHomePressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonPlusPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonMinusPressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonOnePressed;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonTwoPressed;
+
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonAReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonBReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDUpReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDDownReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDLeftReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonDRightReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonHomeReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonPlusReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonMinusReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonOneReleased;
+	UPROPERTY(BlueprintAssignable, Category = "WiimoteWrapper") FWiimoteButtonSignature OnButtonTwoReleased;
+	
+#pragma endregion
+
 #pragma region Functions
 
-	UFUNCTION(BlueprintCallable, Category = "WiimoteWrapper") float ReturnChuj();
+	UFUNCTION(BlueprintCallable, Category = "WiimoteWrapper") FVector WiimoteTilt();
+	UFUNCTION(BlueprintCallable, Category = "WiimoteWrapper") FVector WiimoteRotationRate();
+	UFUNCTION(BlueprintCallable, Category = "WiimoteWrapper") FVector WiimoteAcceleration();
+	UFUNCTION(BlueprintCallable, Category = "WiimoteWrapper") void CalibrateAtRest();
 
 #pragma endregion
 };
